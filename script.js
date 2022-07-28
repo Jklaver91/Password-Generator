34d9120d2ba682bf55940694b70c084205332268
Justin Klaver <Klaverts@yahoo.com>
2022-07-28 19:07:32 -0400

    added random password generator

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  console.log("suh");
-  
-  // passwordText.value = password;
 
   var PassConfirm = window.confirm("Would you like to generate a password?");
         if (PassConfirm) {
-          SpecChar();
+          CapChar();
+    }
+    else {
+      window.alert("You did not pick a valid option. Try again.");
     }
 
 }
@@ -20,51 +17,66 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var CapChar = function() {
+  // ask player what they'd like to do
+   CapitalChar = window.confirm("Would you like to use Capital-case characters?");
+        if (CapitalChar) {
+          console.log(CapitalChar);
+          LowChar();
+    }
+    else {
+      console.log(CapitalChar);
+      LowChar();
+    }
+    
+}
+
+var LowChar = function() {
+  LowerChar = window.confirm("Would you like to use Lower-case characters?");
+        if (LowerChar) {
+          console.log(LowerChar);
+          SpecChar();
+    }
+    else {
+      console.log(LowerChar);
+      SpecChar();
+    }
+
+}
 
 var SpecChar = function() {
   // ask player what they'd like to do
-  var PassConfirm = window.confirm("Would you like to use special characters?");
-        if (PassConfirm) {
+  SpecialChar = window.confirm("Would you like to use special characters?");
+        if (SpecialChar) {
+          console.log(SpecialChar);
           CharacterAmount();
     }
     else {
+      console.log(SpecialChar);
       CharacterAmount();
     }
 }
 
 var CharacterAmount = function() {
   // ask player what they'd like to do
-  var CharacterAmount = window.prompt(
-    "How many characters long do you want your password to be? (6-12)"
+   var CharacterAmount = window.prompt(
+    "How many characters long do you want your password to be? (8-128)"
   );
 
   // convert answer from prompt to an actual number
   CharacterAmount = parseInt(CharacterAmount);
-
-  switch (CharacterAmount) {
-    case 6:
-      console.log("6");
-      break;
-    case 7:
-      console.log("7");
-      break;
-    case 8:
-      console.log("8");
-      break;
-    case 9:
-      console.log("9");
-      break;
-    case 10:
-      console.log("10");
-      break;
-    case 11:
-      console.log("11");
-      break;
-    case 12:
-      console.log("12");
-      break;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      break;
+  console.log(CharacterAmount);
+ 
+    if (SpecialChar && LowerChar && CapitalChar){
+      chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    var passwordLength = CharacterAmount -1;
+    password = "";
+    
+    for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
   }
-}
+
